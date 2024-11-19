c9bfca0707314310d8ca4123a9a15d0f17db7a6d
MisooKim2 <misoo.kim@jnu.ac.kr>
2024-11-19 23:01:01 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function to concatenate two strings
+string concatenate(const std::string& str1, const std::string& str2) {
+    return str1 + str2;
+}
+
+// Main function
+int main() {
+    cout << "Hello, GitHub Actions!" << endl;
+    return 0;
+}
